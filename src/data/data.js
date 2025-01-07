e2e95d194a3b4f3e42759355a006b9b26f33abf0
John Zhang <john.zhang@mercedesandsingh.com>
2025-01-07 08:11:54 -0700

    portfolio 1

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+// CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
+
+const data = {
+    name: "John Doe",
+    bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
+    contact: {
+        email: "john.doe@gmail.com",
+        phone: "+12345678910",
+        linkedin: "https://www.linkedin.com/",
+        github: "https://www.github.com/",
+    }
+}
+
+export default data;
---
+++ src/main.js
@@ -0,0 +1,59 @@
+import './style.css';
+import data from './data/data';
+import { cube } from './3DModel/cube';
+
+const cubes = [
+  './public/cubeDrawings/cube1.webp',
+  './public/cubeDrawings/cube2.webp',
+  './public/cubeDrawings/cube3.webp',
+  './public/cubeDrawings/cube4.webp',
+]
+
+document.querySelector('#app').innerHTML = `
+  <main id="container">
+    <section id="heading">
+      <h1>${data.name}</h1>
+      <p>${data.bio}</p>
+    </section>
+
+    <section id="projects">
+      <div id="project-row">
+        <div id="model1"></div>
+        <div id="images">
+          <div>
+            ${cubes.map((cube, index) => `<img src="${cube}" alt="cube${index + 1}" />`).join('')}
+          </div>
+        </div>
+      </div>
+
+      <div id="project-row">
+        <div id="model2"></div>
+        <div id="images">
+          <div>
+            ${cubes.map((cube, index) => `<img src="${cube}" alt="cube${index + 1}" />`).join('')}
+          </div>
+        </div>
+      </div>
+
+      <div id="project-row">
+        <div id="model3"></div>
+        <div id="images">
+          <div>
+            ${cubes.map((cube, index) => `<img src="${cube}" alt="cube${index + 1}" />`).join('')}
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <ul id="footer-items">
+      <li>Email: ${data.contact.email}</li>
+      <li>Phone: ${data.contact.phone}</li>
+      <li>LinkedIn: <a href="${data.contact.linkedin}">${data.contact.linkedin}</a></li>
+      <li>GitHub: <a href="${data.contact.github}">${data.contact.github}</a></li>
+    </ul>
+  </main>
+`;
+
+cube('model1');
+cube('model2');
+cube('model3');
